7dcc79f2758a64b720ee4d76b931a14d8093292b
Walter4B <walter.bali.markovi@gmail.com>
2022-02-19 21:07:21 +0100

    main SQLtable handling functions

---
+++ Coding Tracker/CodingController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Configuration;
+
+namespace Coding_Tracker
+{
+    internal class CodingController
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["connectionKey"].ConnectionString;
+
+        internal void CreateTable()
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                using (SQLiteCommand command = connection.CreateCommand())
+                { 
+                    connection.Open();
+                    command.CommandText = @"CREATE TABLE IF NOT EXISTS CodingTable (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, Date TEXT NOT NULL, Time TEXT NOT NULL)";
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+        }
+
+        internal void ReadRecords()
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                using (SQLiteCommand command = connection.CreateCommand())
+                {
+                    connection.Open();
+                    command.CommandText = $"SELECT * FROM CodingTable";
+                    using (SQLiteDataReader sqlDataReader = command.ExecuteReader())
+                    {
+                        while (sqlDataReader.Read())
+                        {
+                            Console.WriteLine($"{sqlDataReader.GetInt32(0)} - {sqlDataReader.GetString(1)} - {sqlDataReader.GetString(2)}");
+                        }
+                    }
+                    connection.Close();
+                }
+            }
+        }
+
+        internal void DeleteRecord(int id)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                using (SQLiteCommand command = connection.CreateCommand())
+                {
+                    connection.Open();
+                    command.CommandText = $"DELETE FROM CodingTable WHERE ID = '{id}';";
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+        }
+
+        internal void InsertRecordTesting(int x, int y)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                using (SQLiteCommand command = connection.CreateCommand())
+                {
+                    connection.Open();
+                    command.CommandText = $"INSERT INTO CodingTable (Date, Time) VALUES('{x}','{y}');";
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+        }
+
+    }
+}
--- Coding Tracker/Program.cs
+++ Coding Tracker/Program.cs
@@ -1,11 +1,17 @@
 ﻿using System.Data.SQLite;
 using System.Globalization;
+using Coding_Tracker;
 
 class CodingTracker
 {
     public static void Main()
-    { 
-    
+    {
+        Coding_Tracker.CodingController codingController = new CodingController();
+        codingController.CreateTable();
+        codingController.InsertRecordTesting(1, 20);
+        codingController.ReadRecords();
+        Console.ReadLine();
+
     }
 
 }
