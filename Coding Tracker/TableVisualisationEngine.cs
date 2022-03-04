deeac6eb7e42ea213e3cfda8859a18fd8c2520e0
Walter4B <walter.bali.markovi@gmail.com>
2022-03-04 20:00:46 +0100

    Removing unused

--- Coding Tracker/TableVisualisationEngine.cs
+++ Coding Tracker/TableVisualisationEngine.cs
@@ -4,11 +4,6 @@ namespace Coding_Tracker
 {
     internal class TableVisualisationEngine
     {
-        internal void AddLine(int id, string startTime, string endTime, string sessionTime)
-        {
-            var tableDataLine = new List<object> { id, startTime, endTime, sessionTime };
-        }
-
         internal void DisplayTable(List<List<Object>> ListOfTableLines )
         {
             ConsoleTableBuilder
